3ba825e0d18ff43ab723e4108936f7eaf07d92f4
jay <emma.jordan@bsu.edu>
2022-10-19 13:35:57 -0400

    changed to classes

--- StaticKeyword/untitled/src/Enemy.java
+++ StaticKeyword/untitled/src/Enemy.java
@@ -1,2 +1,25 @@
 public class Enemy {
+    private String name;
+    private int health;
+    private int strength;
+    private static int enemyCount;
+
+    public Enemy(String name, int health, int strength){
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        enemyCount++;
+    }
+    public static int getEnemyCount(){
+        return enemyCount;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getHealth(){
+        return health;
+    }
+    public int getStrength(){
+        return strength;
+    }
 }
--- StaticKeyword/untitled/src/Main.java
+++ StaticKeyword/untitled/src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args){
+        Enemy witch = new Enemy("Witch",13, 30);
+        Enemy hunter = new Enemy("Hunter", 15, 15);
+        System.out.printf("Witch enemy count: %d\n", witch.getEnemyCount());
+        System.out.printf("Hunter enemy count: %d\n", hunter.getEnemyCount());
+        System.out.printf("Enemy enemy count: %d\n", Enemy.getEnemyCount());
+
+
+    }
 }
