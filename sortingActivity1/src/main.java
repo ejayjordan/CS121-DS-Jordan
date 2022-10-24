9a00c17686bac585533cb2b0aa375096f0c5be68
jay <emma.jordan@bsu.edu>
2022-10-24 13:48:10 -0400

    bubble sort

--- sortingActivity1/src/main.java
+++ sortingActivity1/src/main.java
@@ -1,2 +1,6 @@
+import java.util.Arrays;
 public class main {
+    public static void main (String[] args){
+        Sorting sorter = new Sorting();
+    }
 }
--- sortingActivity1/src/sorting.java
+++ sortingActivity1/src/sorting.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class sorting {
-}
+    Scanner console = new Scanner(System.in);
+    ArrayList<Integer> integers = new ArrayList<>();
+    int number = 1;
+
+    public ArrayList<Integer> getArray(){
+        System.out.printf("Enter integer %d:\n", number);
+        int integer = Integer.parseInt(console.nextLine());
+        integers.add(integer);
+        System.out.println();
+        number++;
+        return integers;
+        }
+    public int[] sortArray(int[] integers){
+        int temp;
+        for (int i = 0; i < integers.length - 1; i++){
+            for (int index = 0; index < integers.length - i - 1; index++){
+                if (integers[index] > integers[index + 1]){
+                    temp = integers[index];
+                    integers[index] = integers[index +1];
+                    integers[index + 1] = temp;
+                }
+            }
+        }
+        return integers;
+    }
+
+    }
