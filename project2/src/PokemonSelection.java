933bc386168b07e5cb6353864c82ab45d776e201
jay <emma.jordan@bsu.edu>
2022-11-02 13:53:40 -0400

    edits to Project2

--- project2/src/PokemonSelection.java
+++ project2/src/PokemonSelection.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
 public class PokemonSelection {
+    public createPokemon(String Pokename, int Pokehp, String Pokemove, int Pokepower, int Pokespeed){
+        System.out.println();
+        System.out.println("Enter name:\n");
+        Pokename = Scanner.nextLine();
+        System.out.println("Enter HP:\n");
+        Pokehp = Integer.parseInt(Scanner.nextLine());
+        System.out.println("Enter move:\n");
+        Pokemove = Scanner.nextLine();
+        System.out.println("Enter power:\n");
+        Pokepower = Integer.parseInt(Scanner.nextLine());
+        System.out.println("Enter speed:\n");
+        Pokespeed = Integer.parseInt(Scanner.nextLine());
+    }
+    public assignPokemon(){
+        
+    }
 }
