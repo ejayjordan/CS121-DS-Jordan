3270d4d082da6c2ccd24313146e822cf37ac1c12
jay <emma.jordan@bsu.edu>
2022-11-02 21:13:37 -0400

    update sortingActivity2

--- SortingActivity2/src/Main.java
+++ SortingActivity2/src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Sorting sorter = new Sorting();
 
         int[] x = sorter.getArray();
         System.out.println("Unsorted: ");
         printArray(x);
-
-        int[] y = sorter.sortArray(printArray(x));
+        int[] y = sorter.sortArray(x);
         System.out.println("Sorted: ");
         printArray(y);
 
         System.out.println();
     }
-    public static int[] printArray(int[] integers){
-        for (int number: integers) {
+
+    public static int[] printArray(int[] integers) {
+        for (int number : integers) {
             System.out.print(number + " ");
         }
         System.out.println();
--- SortingActivity2/src/Sorting.java
+++ SortingActivity2/src/Sorting.java
@@ -1,37 +1,31 @@
 import java.util.Scanner;
 
 public class Sorting {
-        Scanner console = new Scanner(System.in);
-        int[] integers;
+    Scanner console = new Scanner(System.in);
+    int[] integers;
 
-        public int[] getArray() {
-            int[] integers = new int[5];
-            for (int i = 0; i < integers.length - 1; i++)
-            {
-                System.out.printf("Enter integer %d: \n", i + 1);
-                int number = Integer.parseInt(console.nextLine());
-                integers[i] = number;
-            }
-            for (int num: integers)
-            {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            return integers;
+    public int[] getArray() {
+        int[] integers = new int[5];
+        for (int i = 0; i < integers.length - 1; i++)
+        {
+            System.out.printf("Enter integer %d: \n", i + 1);
+            int number = Integer.parseInt(console.nextLine());
+            integers[i] = number;
         }
-        public int[] sortArray(int[] integers) {
-            int temp;
-            for (int i = 1; i < integers.length; i++) {
-                for (int j = i; j > 0; j--){
-                    if (integers[j] < integers[j - 1]) {
-                        temp = integers[j];
-                        integers[j] = integers[j -1];
-                        integers[j - 1] = temp;
-                    }
+        return integers;
+    }
+    public int[] sortArray(int[] integers) {
+        int temp;
+        for (int i = 1; i < integers.length; i++) {
+            for (int j = i; j > 0; j--){
+                if (integers[j] < integers[j - 1]) {
+                    temp = integers[j];
+                    integers[j] = integers[j -1];
+                    integers[j - 1] = temp;
                 }
-
             }
-            return integers;
-        }
-}
 
+        }
+        return integers;
+    }
+}
