f7b57cd51a240ad6dfb4d316cdfef2c9eaa76593
jay <emma.jordan@bsu.edu>
2022-11-07 12:48:13 -0500

    pokemon_project2

--- project2/src/PokemonSelection.java
+++ project2/src/PokemonSelection.java
@@ -1,25 +1,39 @@
 import java.util.Scanner;
 public class PokemonSelection {
-    int i;
-    public createPokemon(String Pokename, int Pokehp, String Pokemove, int Pokepower, int Pokespeed){
-        for (i = 0; i < 2; i++)
-            System.out.println();
-            System.out.println("Enter name:\n");
-            Pokename = Scanner.nextLine();
-            System.out.println("Enter HP:\n");
-            Pokehp = Integer.parseInt(Scanner.nextLine());
-            System.out.println("Enter move:\n");
-            Pokemove = Scanner.nextLine();
-            System.out.println("Enter power:\n");
-            Pokepower = Integer.parseInt(Scanner.nextLine());
-            System.out.println("Enter speed:\n");
-            Pokespeed = Integer.parseInt(Scanner.nextLine());
+    Scanner scanner = new Scanner(System.in);
+    public Pokemon createPokemon()
+    {
+        System.out.println();
+        System.out.println("Enter name:\n");
+        // changes from Scanner to scanner lower case s
+        String Pokename = scanner.nextLine();
+        System.out.println("Enter HP:\n");
+        int Pokehp = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter move:\n");
+        String Pokemove = scanner.nextLine();
+        System.out.println("Enter power:\n");
+        int Pokepower = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter speed:\n");
+        int Pokespeed = Integer.parseInt(scanner.nextLine());
+        Pokemon pokemonX = new Pokemon(Pokename,Pokehp,Pokemove,Pokepower,Pokespeed);
+        return pokemonX;
     }
     public void assignPokemon(){
         System.out.println();
-        System.out.printf("Player %d: Select a Pokemon and enter its stats", i);
+        int i = 1;
+        do {
+            System.out.printf("Player %d: Select a Pokemon and enter its stats", i);
 
-
-
+            Pokemon pokemon1 = createPokemon();
+            i++;
+            System.out.printf("Player %d: Select a Pokemon and enter its stats", i);
+            Pokemon pokemon2 = createPokemon();
+            System.out.println("Player " + (i - 1) + " Pokemon");
+            System.out.println("------------------------------");
+            pokemon1.displayPokemonStats();
+            System.out.println("Player " + (i) + " Pokemon");
+            System.out.println("------------------------------");
+            pokemon2.displayPokemonStats();
+        }while(i <2);
     }
 }
