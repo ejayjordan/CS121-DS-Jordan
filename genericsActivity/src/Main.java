2c9301d6bd7f69866ebee960f9e49069e62b892b
jay <emma.jordan@bsu.edu>
2022-11-14 21:54:05 -0500

    genericMethods

--- genericsActivity/src/Main.java
+++ genericsActivity/src/Main.java
@@ -1,2 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args){
+        genericMethods generic = new genericMethods();
+        ArrayList<Integer> integerArrayList = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        ArrayList<Double> doubleArrayList = new ArrayList<>(Arrays.asList(1.1,2.2,3.3,4.4,5.5));
+        ArrayList<Character> characterArrayList = new ArrayList<>(Arrays.asList('A','B','C','D','E'));
+        ArrayList<String> stringArrayList = new ArrayList<>(Arrays.asList("Alpha", "Beta", "Gamma", "Delta", "Epsilon"));
+
+        System.out.print("Integer array: ");
+        generic.printArrayList(integerArrayList);
+        System.out.print("Double array: ");
+        generic.printArrayList(doubleArrayList);
+        System.out.print("Character array: ");
+        generic.printArrayList(characterArrayList);
+        System.out.print("String array: ");
+        generic.printArrayList(stringArrayList);
+    }
 }
--- genericsActivity/src/genericMethods.java
+++ genericsActivity/src/genericMethods.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class genericMethods {
+    public <E> void printArrayList(ArrayList<E> inputArrayList) {
+        for (E element : inputArrayList) {
+            System.out.printf("%s ", element);
+        }
+        System.out.println();
+    }
 }
