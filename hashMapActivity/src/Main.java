77e190cf4ee012326ad548bf8f10b35ee55e8b05
jay <emma.jordan@bsu.edu>
2022-11-14 22:19:07 -0500

    hashmap

--- hashMapActivity/src/Main.java
+++ hashMapActivity/src/Main.java
@@ -1,2 +1,19 @@
+import java.util.HashMap;
+import java.util.Objects;
 public class Main {
+    public static void main(String[] args){
+        hashMap demo = new hashMap();
+
+        demo.addToGradesList("cs121", 98);
+        demo.addToGradesList("cs124", 96);
+        demo.addToGradesList("cs222", 89);
+
+        demo.displayGrades();
+
+        System.out.println(demo.getScore("cs121"));
+        demo.removeFromGradesList("cs121");
+        System.out.println(demo.getScore("cs121"));
+
+        demo.displayGrades();
+    }
 }
--- hashMapActivity/src/hashMap.java
+++ hashMapActivity/src/hashMap.java
@@ -1,2 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
 public class hashMap {
+    private Map<String, Integer> gradesMap = new HashMap<>();
+
+    public void addToGradesList(String course, int score){
+        gradesMap.put(course, score);
+    }
+
+    public void removeFromGradesList(String course) {
+        gradesMap.remove(course);
+    }
+
+    public Object getScore(String course){
+        Object score;
+        if(gradesMap.containsKey(course)){
+            score = gradesMap.get(course);
+        } else {
+            score = "Course not found.";
+        }
+        return score;
+    }
+
+    public void displayGrades(){
+        gradesMap.forEach( (course, score) -> {
+            System.out.printf("Course: %s | Grade: %d\n", course, score);
+        });
+    }
 }
