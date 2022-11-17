7f1dbf76fb02d165e34e4d3ccadae2670adada07
jay <emma.jordan@bsu.edu>
2022-11-17 14:35:20 -0500

    linkedListActivity

---
+++ linkedListActivity/src/Student.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class Student {
+    public String firstName;
+    public String lastName;
+    public Integer ID;
+    public String major;
+    public String classStanding;
+    public Double GPA;
+    public LinkedList<String> courseList = new LinkedList<>("cs121", "cs122", "cs222");
+
+    
+}
