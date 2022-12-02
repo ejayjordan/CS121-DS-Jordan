fda9884be73d7dfbd79e04dcbfff2ef7a1db0ff9
jay <emma.jordan@bsu.edu>
2022-12-02 13:41:25 -0500

    stringBuilderActivity

--- testActivity/src/PokemonTest.java
+++ testActivity/src/PokemonTest.java
@@ -1,12 +1,17 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 class PokemonTest {
+    private Pokemon pokemon = new Pokemon("Squirtle", 10,"Tackle", 60, 30);
 
     @org.junit.jupiter.api.Test
     void getPokename() {
+        //assertEquals("Charmander", pokemon.getPokename());
+        assertEquals("Squirtle", pokemon.getPokename());
     }
 
     @org.junit.jupiter.api.Test
     void getPokemove() {
+        //assertEquals("Water Beam", pokemon.getPokemove());
+        assertEquals("Tackle", pokemon.getPokemove());
     }
 }
