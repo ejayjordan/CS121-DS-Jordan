9cf65a7f157270b17e2c2624e4103d38c2518368
jay <emma.jordan@bsu.edu>
2022-12-02 13:28:51 -0500

    testActivity

---
+++ testActivity/src/PokemonTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PokemonTest {
+
+    @org.junit.jupiter.api.Test
+    void getPokename() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPokemove() {
+    }
+}
