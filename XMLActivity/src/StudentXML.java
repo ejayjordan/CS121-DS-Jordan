d2a4c2b010da946e378995d22583783f5fb3fbe1
jay <emma.jordan@bsu.edu>
2022-12-10 12:33:03 -0500

    XMLActivity

--- XMLActivity/src/StudentXML.java
+++ XMLActivity/src/StudentXML.java
@@ -1,12 +1,10 @@
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class StudentXML {
     public static void main(String[] args) {
@@ -16,32 +14,20 @@ public class StudentXML {
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse(input);
 
-            System.out.println("Root element: " +
-                    doc.getDocumentElement().getNodeName());
-            NodeList personList = doc.getElementsByTagName("student");
-            for (int i = 0; i < personList.getLength(); i++) {
-                Node node = personList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+            NodeList studentList = doc.getElementsByTagName("student");
+            for(int i = 0; i < studentList.getLength(); i++){
+                Node node = studentList.item(i);
+                if(node.getNodeType() == Node.ELEMENT_NODE){
                     Element element = (Element) node;
                     System.out.println("ID: " + element.getAttribute("id"));
-                    System.out.println("First Name: " + element
-                            .getElementsByTagName("firstname")
-                            .item(0)
-                            .getTextContent());
-                    System.out.println("Last Name: " + element
-                            .getElementsByTagName("lastname")
-                            .item(0)
-                            .getTextContent());
-                    System.out.println("GPA: " + element
-                            .getElementsByTagName("gpa")
-                            .item(0)
-                            .getTextContent());
-                    System.out.println("Major: " + element
-                            .getElementsByTagName("major")
-                            .item(0)
-                            .getTextContent());
+                    System.out.println("First Name: " + element.getElementsByTagName("firstname").item(0).getTextContent());
+                    System.out.println("Last Name: " + element.getElementsByTagName("lastname").item(0).getTextContent());
+                    System.out.println("GPA: " + element.getElementsByTagName("gpa").item(0).getTextContent());
+                    System.out.println("Major: " + element.getElementsByTagName("major").item(0).getTextContent());
                     System.out.println();
                 }
+
             }
         }catch (Exception e) {
             e.printStackTrace();
